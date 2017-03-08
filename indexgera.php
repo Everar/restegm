f66ab15fb0156a922104cd48cc6c0ba7dee91589
Everar <1715110317@utec-tgo.edu.mx>
2017-03-08 16:33:40 -0600

    Add files via upload

---
+++ indexgera.php
@@ -0,0 +1,19 @@
+<?php
+   $db_host = "am1shyeyqbxzy8gc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+   $db_name = "azrd3bql1guetrj0";
+   $db_user = "r6dakcllmlu9yrvx";
+   $db_password = "plvyd1itkfzooknr";
+    
+  $connection = mysqli_connect('am1shyeyqbxzy8gc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'r6dakcllmlu9yrvx', 'plvyd1itkfzooknr');
+    
+    mysqli_select_db($connection, $db_name) or die("Error al seleccionar la base de datos:".mysqli_error());
+    @mysqli_query("SET NAMES 'utf8'");
+
+   $sql_query = "SELECT * FROM contactos;";
+   $result = mysqli_query($sql_query);
+   $rows = array();
+while($r = mysqli_fetch_assoc($result)) {
+  $rows[] = $r;
+}
+print json_encode($rows);
+?>
